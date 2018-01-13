89eae98808b1a6d27d9ee7193f1f1a7fd770ec16
fengtao.xue <fengtao.xue@gausscode.com>
2018-01-13 10:27:37 +0800

    add hdfsUtil

--- src/main/java/com/rbs/dim/DimMapper.java
+++ src/main/java/com/rbs/dim/DimMapper.java
@@ -12,15 +12,18 @@ import java.io.IOException;
  */
 public class DimMapper extends Mapper<LongWritable, Text, Text, Text>{
 
+    Dim dim = new Dim();
+    Text k = new Text();
+    Text v = new Text();
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         //130	1300000	山东	济南	联通	250000	0531	370100	中国联通 GSM
-        String[] tokens = StringUtils.SPARATOR.split(value.toString());
-        Text k = new Text();
-        Text v = new Text();
+        //String[] tokens = StringUtils.SPARATOR.split(value.toString());
+        dim = Dim.parsePhoneRecord(value.toString());
 
-        k.set(tokens[0]);
-        v.set(tokens.toString());
+        k.set(dim.getIsp());
+        v.set(dim.getPre_7_num());
         context.write(k,v);
 
     }
--- src/main/java/com/rbs/dim/DimReducer.java
+++ src/main/java/com/rbs/dim/DimReducer.java
@@ -1,6 +1,6 @@
 package com.rbs.dim;
 
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -9,13 +9,17 @@ import java.io.IOException;
 /**
  * Created by fengtao.xue on 2018/1/13.
  */
-public class DimReducer extends Reducer<Text, Text, Text, Text>{
+public class DimReducer extends Reducer<Text, Text, Text, IntWritable>{
+
+    IntWritable resCount = new IntWritable();
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Integer sum = 0;
         for (Text v : values){
-
+            sum += 1;
         }
+        resCount.set(sum);
+        context.write(key, resCount);
     }
 }
